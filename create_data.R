4310358e445d074e7f935a2528bbe12853b8d46c
JulienMir <jumiconnais@hotmail.no>
2018-11-28 13:52:47 +0100

    Encapsulation

--- create_data.R
+++ create_data.R
@@ -1,55 +1,29 @@
-raw <- " Remember, remember!
-    The fifth of November,
-The Gunpowder treason and plot;
-I know of no reason
-Why the Gunpowder treason
-Should ever be forgot!
-Guy Fawkes and his companions
-Did the scheme contrive,
-To blow the King and Parliament
-All up alive.
-Threescore barrels, laid below,
-To prove old England's overthrow.
-But, by God's providence, him they catch,
-With a dark lantern, lighting a match!
-A stick and a stake
-For King James's sake!
-    If you won't give me one,
-I'll take two,
-    The better for me,
-    And the worse for you.
-    A rope, a rope, to hang the Pope,
-    A penn'orth of cheese to choke him,
-A pint of beer to wash it down,
-And a jolly good fire to burn him.
-Holloa, boys! holloa, boys! make the bells ring!
-Holloa, boys! holloa boys! God save the King!
-Hip, hip, hooor-r-r-ray!"
-
-corpus <- tm::MC_tokenizer(raw)
-
-dict <- unique(corpus)
-
-# Largeur de la fenêtre contexte
-l <- 3
-
-# Rajout de mot vide au début et à la fin
-corpus <- c(rep("<>NULL<>", l),
-            corpus,
-            rep("<>NULL<>", l))
-
-D <- matrix(NA, nrow = length(corpus)-2*l, ncol = 1+2*l)
-colnames(D) <- c("target", paste0("context_", 1:(2*l)))
-
-# Construction du jeu de données D
-for(w in 1:(length(corpus)-2*l)){
-  D[w,] <- c(corpus[w+l], corpus[w+l-(1:l)], corpus[w+l+(1:l)])
-}
-
-# Transformation des mots en indices
-for(ligne in 1:nrow(D)){
-  for(col in 1:ncol(D)){
-    D[ligne, col] <- which(D[ligne, col] == dict)
+create_data <- function(rawText, l) {
+  corpus <- tm::MC_tokenizer(rawText)
+  
+  # Rajout de mot vide au début et à la fin
+  corpus <- c(rep("<>NULL<>", l),
+              corpus,
+              rep("<>NULL<>", l))
+  
+  dict <- unique(corpus)
+  
+  D <- matrix(NA, nrow = length(corpus)-2*l, ncol = 1+2*l)
+  colnames(D) <- c("target", paste0("context_", 1:(2*l)))
+  
+  # Construction du jeu de données D
+  for(w in 1:(length(corpus)-2*l)){
+    D[w,] <- c(corpus[w+l], corpus[w+l-(1:l)], corpus[w+l+(1:l)])
   }
+  
+  # Transformation des mots en indices
+  Dp <- matrix(rep(0, ncol(D)*nrow(D)), ncol = ncol(D))
+  for(ligne in 1:nrow(D)){
+    for(col in 1:ncol(D)){
+      Dp[ligne, col] <- which(D[ligne, col] == dict)
+    }
+  }
+  Dp <- apply(Dp, 2, as.numeric)
+
+  return(list(D = Dp, vocab = dict))
 }
-D <- apply(D, 2, as.numeric)
--- sgd.R
+++ sgd.R
@@ -1,52 +1,54 @@
-# Initialisation de U et V
-# nombre de dimensions
-p <- 10
-n <- length(dict)
-U <- matrix(runif(n*p), nrow = n, ncol = p)
-V <- matrix(runif(n*p), nrow = n, ncol = p)
+source("gradient.R")
+source("softmax.R")
 
-# Pas d'apprentissage
-eta <- 0.025
-
-
-
-# Boucle
-n_iter <- 1
-for(iter in 1:n_iter){
+sgd_CBOW <- function(D, vocab, p, n_iter) {
+  # Initialisation de U et V
+  n <- length(vocab)
+  U <- matrix(runif(n*p), nrow = n, ncol = p)
+  V <- matrix(runif(n*p), nrow = n, ncol = p)
   
-  # Mélange de D
-  order <- sample(1:nrow(D), nrow(D), replace = F)
-  D <- D[order, ]
+  # Pas d'apprentissage
+  eta <- 0.025
   
-  # Boucle sur les couples mot_cible/contexte
-  for(row in 1:nrow(D)){
+  # Boucle
+  for(iter in 1:n_iter){
     
-    # ID du mot cible
-    i <- D[row, 1]
+    # Mélange de D
+    order <- sample(1:nrow(D), nrow(D), replace = F)
+    Dp <- D[order, ]
     
-    # ID des mots contexte
-    j <- D[row, -1]
-    
-    # Calcul du alpha contexte
-    alpha_c <- apply(V[j,], 2, sum)/(2*l)
-    
-    
-    # Calcul des Softmax
-    softmax <- fun_softmax(U, alpha_c)
-    
-    # MAJ de Ui
-    U[i,] <- U[i,] + eta * grad_u(U, V, softmax[i], alpha_c)
-    
-    # Boucle sur les mots contexte
-    for(word in 1:(ncol(D)-1)){
+    # Boucle sur les couples mot_cible/contexte
+    for(row in 1:nrow(D)){
       
       # ID du mot cible
-      jl <- D[row, word + 1]
+      i <- Dp[row, 1]
       
-      # MAJ de Vjl
-      V[jl,] <- V[jl,] + eta * grad_v(U, V, i, softmax, l)
+      # ID des mots contexte
+      j <- Dp[row, -1]
+      
+      # Calcul du alpha contexte
+      alpha_c <- apply(V[j,], 2, sum)/(2*l)
+      
+      
+      # Calcul des Softmax
+      softmax <- fun_softmax(U, alpha_c)
+      
+      # MAJ de Ui
+      U[i,] <- U[i,] + eta * grad_u(U, V, softmax[i], alpha_c)
+      
+      # Boucle sur les mots contexte
+      for(word in 1:(ncol(D)-1)){
+        
+        # ID du mot cible
+        jl <- Dp[row, word + 1]
+        
+        # MAJ de Vjl
+        V[jl,] <- V[jl,] + eta * grad_v(U, V, i, softmax, l)
+      }
     }
   }
+  
+  return(U)
 }
 
 
