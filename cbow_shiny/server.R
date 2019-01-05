2d1ff22ceffd1f6e21ec802bcc462cd3ceef0c7b
JulienMir <jumiconnais@hotmail.no>
2019-01-05 19:06:01 +0100

    Final push

--- cbow_shiny/server.R
+++ cbow_shiny/server.R
@@ -2,44 +2,53 @@ library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(dplyr)
+library(doParallel)
 library(ggplot2)
 
 shinyServer(server <- function(input, output, session) {
   
   ######################## INITIALIZATION ########################
-  
-  # Load learned representations
+  ### Load learned representations
   # Found at https://vsmlib.readthedocs.io/en/latest/tutorial/getting_vectors.html#pre-trained-vsms
-  models <- vector("list", 2)
-  modelnames <- c("CBOW Unbound", "SkipGram Unbound")
-  paths <- c("./data/word_deps_cbow_25d/", "./data/word_deps_sg_25d/")
+  modelnames <- c("CBOW 25d", "SkipGram 25d", "CBOW 500d", "SkipGram 500d")
+  paths <- c("./data/word_deps_cbow_25d/",
+             "./data/word_deps_sg_25d/",
+             "./data/word_deps_cbow_500d/",
+             "./data/word_deps_sg_500d/")
+  models <- vector("list", length(paths))
   
-  for(i in 1:2) {
-    # Lecture du fichier NPY converti
-    con <- file(paste0(paths[i],"words25.bin"), "rb")
-    
-    dim <- readBin(con, "integer", 2)
-    words <- matrix(readBin(con, "numeric", prod(dim)), dim[1], dim[2])
-    
-    close(con)
-    
-    vocab <- readLines(paste0(paths[i],"words25.vocab"))
-    
-    models[[i]] <- list(vectors=words, vocab=vocab)
+  for(i in 1:length(paths)) {
+    models[[i]] <- list(vectors = as.matrix(read.csv2(paste0(paths[i], "words.csv"))), 
+                        vocab = as.character(read.csv2(paste0(paths[i], "vocab.csv"))[,1]))
   }
+  
   
   ######################## GENERAL FUNCTIONS ########################
   cosine_similarity <- function(a, b) {
     return(sum(a*b)/(sqrt(sum(a*a)) * sqrt( sum( b*b ) ) ))
   }
   
-  closest_words <- function(a, model, n=5) {
+  closest_words <- function(a, model, n=5, parallel=FALSE) {
+    if(length(a) == 0) {
+      return(rep("ERROR", n))
+    }
+    
     similarity <- numeric(length(model$vocab))
-    for(i in 1:length(model$vocab)) {
-      v1 <- a
-      v2 <- model$vectors[which(model$vocab == model$vocab[i])]
+    
+    if(parallel) {
+      registerDoParallel(3)
       
-      similarity[i] <- cosine_similarity(v1, v2)
+      similarity <- foreach(i = 1:length(model$vocab), .combine = "c", .export = "cosine_similarity") %dopar%
+        cosine_similarity(a, model$vectors[which(model$vocab == model$vocab[i]), ])
+      
+      stopImplicitCluster()
+    }
+    else {
+      for(i in 1:length(model$vocab)) {
+        v <- model$vectors[which(model$vocab == model$vocab[i]), ]
+        
+        similarity[i] <- cosine_similarity(a, v)
+      }
     }
     
     ordered <- model$vocab[order(-similarity)]
@@ -50,138 +59,245 @@ shinyServer(server <- function(input, output, session) {
   session$onSessionEnded(stopApp)
   
   ######################## MODEL ########################
-  ### Autocomplétion
-  # Predicts the next possibles words when the text input changes
-  # get_next_words <- reactive({
-  #   sentence <- tolower(input$sentence)
-  # 
-  #   if (nchar(sentence) == 0){
-  #     # Empty df
-  #     df <- data.frame(word = character(0L),
-  #                      prob = numeric(0L))
-  #   }
-  #   else{
-  #     sentence <- unlist(strsplit(sentence, "[ ]+"))
-  # 
-  #     # print(sentence)
-  # 
-  #     for(model in models) {
-  #       res <- numeric(0)
-  # 
-  #       for(token in sentence) {
-  #         # print(paste(token, " and ", vocab[which(model$vocab ==  token)]))
-  #         res <- res + model$vectors[which(model$vocab ==  token)]
-  #       }
-  # 
-  #       df <- cbind(df, data.frame(closest_words(res, model, 3)))
-  #     }
-  #   }
-  # 
-  #   colnames(df) <- modelnames
-  # 
-  #   # print(df)
-  # 
-  #   return(df)
-  # })
-  
   ### Analogies
   # Find the result of arthimetic operation on representation vectors
-  find_analogy <- reactive({
-    analogy1 <- input$analogy1
-    analogy2 <- input$analogy2
-    analogy3 <- input$analogy3
-
-    if (nchar(analogy1) == 0 || nchar(analogy2) == 0 || nchar(analogy3) == 0){
-      # Empty df
-      df <- data.frame(word = character(0L),
-                       prob = numeric(0L))
-    }else{
-      words <- c("red", "white", "black", "blue", "orange", "green", "yellow")
-      val <- runif(length(words))
-      df <- data.frame(word = words, prob = val/sum(val)) %>%
-        arrange(desc(prob)) %>%
-        filter(row_number() <= 3)
+  find_analogy <- eventReactive(input$ana_button, {
+    
+    selected_models <- as.integer(input$ana_model_choice)
+    
+    analogy1 <- tolower(input$analogy1)
+    analogy2 <- tolower(input$analogy2)
+    analogy3 <- tolower(input$analogy3)
+    
+    df <- NULL
+    
+    for(mod in selected_models) {
+      model <- models[[mod]]
+      res <- rep(0, ncol(model$vectors))
+      
+      res <- res + model$vectors[which(model$vocab ==  analogy1), ]
+      res <- res - model$vectors[which(model$vocab ==  analogy2), ]
+      res <- res + model$vectors[which(model$vocab ==  analogy3), ]
+      
+      df <- cbind(df, closest_words(res, model, 3))
     }
+    
+    colnames(df) <- modelnames[selected_models]
+    
     return(df)
   })
   
   ### Visualization
-  # Makes a list containing all the data needed for the graph, updates when button visu_button is clicked
+  # Makes a df containing all the data needed for the graph, updates when button visu_button is clicked
   get_coords <- eventReactive(input$visu_button, {
 
     # word tokenisation (same order as input)
-    words <- strsplit(tolower(input$visu_text), " ")[[1]]
+    words <- strsplit(gsub("[^a-z]+", " ", tolower(input$visu_text)), " ")[[1]]
 
-    # Index of the chosen model
-    mod <- as.integer(input$visu_model_choice)
+    # Index of the chosen models
+    selected_models <- as.integer(input$visu_model_choice)
+    
+    coords <- data.frame(PC1 = numeric(0), PC2 = numeric(0), model = character(0), word = character(0))
+    
+    for(mod in selected_models){
+      # Find words indexes (same order as in vocab)
+      index <- which(models[[mod]]$vocab %in% words)
+      
+      # Reorder words (sorting by alphabetical order should be enough but that way there is no doubt)
+      words <- models[[mod]]$vocab[index]
+      
+      # Getting the representations of the selected words
+      repr <- models[[mod]]$vectors[index,]
+      
+      # Performs a PCA and get the vectors coordonates for the first 2 axis
+      # df <- as.data.frame(prcomp(repr)$x[, 1:2]) %>%
+      #   mutate(model = modelnames[mod],
+      #          word = words)
+      
+      df <- as.data.frame(prcomp(models[[mod]]$vectors)$x[, 1:2]) %>%
+        filter(row_number() %in% index) %>%
+        mutate(model = modelnames[mod],
+               word = words)
+      
+      # df <- data.frame(PC1 = repr[,2], PC2 = repr[,2]) %>%
+      #   mutate(model = modelnames[mod],
+      #          word = words)
+      
+      coords <- rbind(coords, df)
+    }
+  
+    return(coords)
+  })
+  
+  ### key word
+  # find the nearest words from the sum of the input : "france" + "capital" = "Paris" (hopefully)
+  get_key_word <- eventReactive(input$key_button, {
+    
+    # word tokenisation (same order as input)
+    words <- unlist(strsplit(tolower(input$key_text), " "))
+    
+    # Index of the chosen models
+    selected_models <- as.integer(input$key_model_choice)
+    
+    i <- 1
+    for(mod in selected_models){
+      # Find words indexes
+      index <- which(models[[mod]]$vocab %in% words)
 
-    # Find words indexes (same order as in vocab)
-    index <- which(models[[mod]]$vocab %in% words)
-
-    # Reorder words (sorting by alphabetical order should be enough but that way there is no doubt)
-    words <- models[[mod]]$vocab[index]
-
-    # Getting the representations of the selected words
-    repr <- models[[mod]]$vectors[index,]
-
-    # Performs a PCA and get the vectors coordonates for the first 3 axis
-    coords <- as.data.frame(prcomp(repr)$x[, 1:3])
-
-    return(list(modelname = modelnames[mod],
-                words = words,
-                coords = coords))
+      # Getting the representations of the selected words
+      repr <- models[[mod]]$vectors[index,]
+      
+      # Sum of all representations
+      sum <- apply(repr, 2, sum)
+      
+      # nearest words from sum
+      nearest <- closest_words(sum, models[[mod]], 3)
+      
+      if(i == 1){
+        df <- data.frame(c1 = nearest)
+      }else{
+        df[,i] <- nearest
+      }
+      i <- i+1
+    }
+    
+    names(df) <- modelnames[selected_models]
+    
+    return(df)
+  })
+  
+  ### likelyhood
+  # returns the mean of cosine similarity of every couple of input words
+  # So it is a score, the higher it is, the similar are the words
+  get_likelyhood <- function(words, selected_models){
+    
+    df <- data.frame(score = numeric(0), model = character(0))
+    
+    for(mod in selected_models){
+      # Find words indexes
+      index <- which(models[[mod]]$vocab %in% words)
+      
+      # Getting the representations of the selected words
+      repr <- models[[mod]]$vectors[index,]
+      
+      # cosinus similarities of every couple of words
+      similarities <- c()
+      for(i in 1:(nrow(repr)-1)){
+        for(j in (i+1):nrow(repr)){
+          similarities <- c(similarities, cosine_similarity(repr[i,], repr[j,]))
+        }
+      }
+      
+      temp <- data.frame(score = mean(similarities), model = modelnames[mod])
+      print(temp)
+      df <- rbind(df, temp)
+    }
+    return(df)
+  }
+  likelyhood1 <- eventReactive(input$like_button, {
+    
+    # word tokenisation (same order as input)
+    words <- strsplit(tolower(input$like_text1), " ")[[1]]
+    
+    # Index of the chosen models
+    selected_models <- as.integer(input$like_model_choice)
+    
+    df <- get_likelyhood(words, selected_models)
+    return(df)
+  })
+  likelyhood2 <- eventReactive(input$like_button, {
+    
+    # word tokenisation (same order as input)
+    words <- strsplit(tolower(input$like_text2), " ")[[1]]
+    
+    # Index of the chosen models
+    selected_models <- as.integer(input$like_model_choice)
+    
+    df <- get_likelyhood(words, selected_models)
+    return(df)
   })
   
   ######################## CONTROLLER ########################
   ### TABLES
-  output$next_word_table <- renderTable({
-    get_next_words()
-  })
-  
   output$analogies_word_table <- renderTable({
-    find_analogy() %>%
-      rename("Résultats" = word,
-             "Probabilité" = prob)
+    find_analogy()
   })
   
-  output$keyword_table <- renderTable({
-    find_analogy() %>%
-      rename("Résultats" = word,
-             "Probabilité" = prob)
+  output$key_word_table <- renderTable({
+    get_key_word()
+  })
+  
+  output$like_table1 <- renderTable({
+    likelyhood1() %>%
+      rename("Score" = score,
+             "Modèle" = model)
+  })
+  output$like_table2 <- renderTable({
+    likelyhood2() %>%
+      rename("Score" = score,
+             "Modèle" = model)
   })
   
   ### GRAPHS
   # Updated whenever get_coords() is updated
   output$visu_graph <- renderPlot({
-
-    data <- get_coords()
+    
+    coords <- get_coords()
     
     ### Graph settings
     # decalage graphique
     dec <- 0.1
     
     # limits
-    xmargin <- (max(data$coords$PC1) - min(data$coords$PC1))*0.08
-    ymargin <- (max(data$coords$PC2) - min(data$coords$PC2))*0.02
-    xlim <- c(min(data$coords$PC1)-xmargin, max(data$coords$PC1)+xmargin)
-    ylim <- c(min(data$coords$PC2)-ymargin, max(data$coords$PC2)+ymargin)
+    xmargin <- (max(coords$PC1) - min(coords$PC1))*0.08
+    ymargin <- (max(coords$PC2) - min(coords$PC2))*0.02
+    xlim <- c(min(0, min(coords$PC1)-xmargin), max(0, max(coords$PC1)+xmargin))
+    ylim <- c(min(0, min(coords$PC2)-ymargin), max(0, max(coords$PC2)+ymargin))
+    nrow <- ceiling(length(unique(coords$model))/2)
     
-    
-    ggplot() +
-      # geom_vline(xintercept = 0) +
-      # geom_hline(yintercept = 0) +
-      geom_text(aes(x = data$coords$PC1, y = data$coords$PC2, label = data$words,
-                    hjust = ifelse(data$coords$PC1 >= 0, -1*dec, 1+dec), vjust = ifelse(data$coords$PC2 >= 0, -1*dec, 1+dec))) +
-      geom_segment(aes(x = 0, y = 0, xend = data$coords$PC1, yend = data$coords$PC2), arrow = arrow()) +
-      # theme_bw() +
-      scale_x_continuous(breaks = NULL, limits = xlim) +
-      scale_y_continuous(breaks = NULL, limits = ylim) +
-      labs(title = paste("Représentation des vecteurs de", data$modelname),
-           x = "1er axe de l'ACP",
-           y = "2ème axe de l'ACP") +
-      theme(plot.title = element_text(hjust = 0.5))
-
-
+    if(!input$type_graph){
+      # bidouillage de la mort pour essayer de rendre les graph multiples plus lisibles
+      # Ca ne change rien à l'interprétation des graph
+      # Il est déconseillé d'essayer de le comprendre
+      mods <- unique(coords$model)
+      if (length(mods) > 1){
+        ex <- sign(max(coords$PC1[coords$model == mods[1]]))
+        for(i in 2:length(mods)){
+          if(sign(max(coords$PC1[coords$model == mods[i]])) != ex){
+            coords$PC1[coords$model == mods[i]] <- coords$PC1[coords$model == mods[i]] * (-1)
+          }
+        }
+      }
+      
+      # Graph
+      ggplot(coords) +
+        geom_point(aes(x = 0, y = 0), shape = 3) + 
+        geom_text(aes(x = PC1, y = PC2, label = word,
+                      hjust = ifelse(PC1 >= 0, -1*dec, 1+dec), vjust = ifelse(PC2 >= 0, -1*dec, 1+dec))) +
+        geom_segment(aes(x = 0, y = 0, xend = PC1, yend = PC2), arrow = arrow()) +
+        facet_wrap( ~ model, nrow = nrow) + theme(legend.position = "none") +
+        # scale_color_manual(values = c("CBOW Unbound" = "#FF6437", "SkipGram Unbound" = "#183FCC")) + 
+        scale_x_continuous(breaks = NULL) +
+        scale_y_continuous(breaks = NULL) +
+        labs(title = "Représentation des vecteurs",
+             x = "1er axe de l'ACP",
+             y = "2ème axe de l'ACP",
+             color = "Modèle") +
+        theme(plot.title = element_text(hjust = 0.5))
+    }else{
+      ggplot(coords) +
+        geom_point(aes(x = 0, y = 0), shape = 3) +
+        geom_text(aes(x = PC1, y = PC2, label = word, color = model,
+                      hjust = ifelse(PC1 >= 0, -1*dec, 1+dec), vjust = ifelse(PC2 >= 0, -1*dec, 1+dec))) +
+        geom_segment(aes(x = 0, y = 0, xend = PC1, yend = PC2, color = model), arrow = arrow()) +
+        # scale_color_manual(values = c("CBOW Unbound" = "#FF6437", "SkipGram Unbound" = "#183FCC")) +
+        scale_x_continuous(breaks = NULL, limits = xlim) +
+        scale_y_continuous(breaks = NULL, limits = ylim) +
+        labs(title = "Représentation des vecteurs",
+             x = "1er axe de l'ACP",
+             y = "2ème axe de l'ACP",
+             color = "Modèle") +
+        theme(plot.title = element_text(hjust = 0.5))
+    }
   })
-
 })
--- cbow_shiny/ui.R
+++ cbow_shiny/ui.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(shinydashboard)
-library(plotly)
 
 page <- dashboardPage(
   
@@ -14,21 +13,6 @@ page <- dashboardPage(
   
   ################### BODY ###################
   dashboardBody(
-    # Fenetre d'es analogies d'auto complétion
-    box(title = "Autocomplétion",
-        status = "primary",
-        width = 12,
-        solidHeader = T,
-        collapsible = T,
-        # box content :
-        fluidRow(
-          column(width = 7,
-                 textInput(inputId = "sentence",
-                           label = h3("Veuillez écrire une phrase"),
-                           value = "My favourite color is")),
-          column(width = 5,
-                 tableOutput("next_word_table"))
-          )),
     # Fenetre des analogies
     box(title = "Analogies",
         status = "primary",
@@ -37,37 +21,82 @@ page <- dashboardPage(
         collapsible = T,
         # box content :
         fluidRow(
-          column(width = 3,
+          column(width = 2,
+                 checkboxGroupInput("ana_model_choice", label = "Choix des modèles", 
+                                    choices = list("CBOW 25d" = 1, "SkipGram 25d" = 2, "CBOW 500d" = 3, "SkipGram 500d" = 4),
+                                    selected = 1)),
+          column(width = 2,
                  textInput(inputId = "analogy1",
-                           label = h3("Mot 1 - "),
-                           value = "Mot 1")),
-          column(width = 3,
+                           label = "Mot 1 - ",
+                           value = "mother")),
+          column(width = 2,
                  textInput(inputId = "analogy2",
-                           label = h3("Mot 2 + "),
-                           value = "Mot 2")),
-          column(width = 3,
+                           label = "Mot 2 + ",
+                           value = "father")),
+          column(width = 2,
                  textInput(inputId = "analogy3",
-                           label = h3("Mot 3 = "),
-                           value = "Mot 3")),
+                           label = "Mot 3 = ",
+                           value = "daughter")),
+          column(width = 1, style = "margin-top: 24px;",
+                 actionButton("ana_button", "Valider")),
           column(width = 3,
                  tableOutput("analogies_word_table"))
         )),
-    # Fenetre des mot-clefs
-    box(title = "Mot-clefs",
+    # Fenetre des key word
+    box(title = "Complétion de requête",
         status = "primary",
         width = 12,
         solidHeader = T,
         collapsible = T,
         # box content :
         fluidRow(
-          column(width = 7,
-                 textInput(inputId = "keyword1",
-                           label = h3("Entre votre texte"),
-                           value = "Capital France")),
+          column(width = 2,
+                 checkboxGroupInput("key_model_choice", label = "Choix des modèles", 
+                                    choices = list("CBOW 25d" = 1, "SkipGram 25d" = 2, "CBOW 500d" = 3, "SkipGram 500d" = 4),
+                                    selected = 1)),
+          column(width = 6,
+                 textInput(inputId = "key_text",
+                           label = "Veuillez écrire une requête",
+                           value = "capital france")),
+          column(width = 1, style = "margin-top: 24px;",
+                 actionButton("key_button", "Valider")),
           column(width = 3,
-                 tableOutput("keyword_table"))
+                 tableOutput("key_word_table"))
         )),
-    uiOutput("moreControls"),
+    # Fenêtre de la vraisemblance
+    box(title = "Vraisemblance",
+        status = "primary",
+        width = 12,
+        solidHeader = T,
+        collapsible = T,
+        # box content :
+        fluidRow(
+          column(width = 2,
+                 checkboxGroupInput("like_model_choice", label = "Choix des modèles", 
+                                    choices = list("CBOW 25d" = 1, "SkipGram 25d" = 2, "CBOW 500d" = 3, "SkipGram 500d" = 4),
+                                    selected = 1:2)),
+          column(width = 6,
+                 textAreaInput(inputId = "like_text1",
+                               label = "Veuillez écrire une phrase",
+                               value = "I love eating at my best friend 's restaurant during my lunch break, the waiters are nice and the cook is tasty",
+                               height = "55px")),
+          column(width = 1, style = "margin-top: 24px;",
+                 actionButton("like_button", "Valider")),
+          column(width = 3,
+                 tableOutput("like_table1"))
+        ),
+        fluidRow(
+          column(width = 2),
+          column(width = 6,
+                 textAreaInput(inputId = "like_text2",
+                               label = "Et une seconde si vous voulez comparer les scores de vraisemblance",
+                               value = "My classification tooth and my belt 's philosophy dishwasher are late at the cemetery",
+                               height = "55px")),
+          column(width = 1),
+          column(width = 3,
+                 tableOutput("like_table2"))
+        )),
+    
     # Fenêtre de la visualisation des projections
     box(title = "Visualisation des projections",
         status = "primary",
@@ -76,18 +105,22 @@ page <- dashboardPage(
         collapsible = T,
         # box content :
         fluidRow(
-          column(width = 4,
+          column(width = 3,
                  column(width = 12,
-                        textInput(inputId = "visu_text",
-                                  label = "Saisissez les mots à visualiser",
-                                  value = "car bike cat dog raspberry strawberry")),
+                        textAreaInput(inputId = "visu_text",
+                                      label = "Saisissez les mots à visualiser",
+                                      value = "mother father daughter son\ncapital france paris italy roma",
+                                      height = "55px")),
                  column(width = 9,
-                         selectInput("visu_model_choice", label = "Choix du modèle", multiple = FALSE,
-                                     choices = list("CBOW Unbound" = 1, "SkipGram Unbound" = 2),
-                                     selected = 1)),
-                 column(width = 3, style = "margin-top: 24px;",
-                         actionButton("visu_button", "Valider"))),
-          column(width = 8,
+                        checkboxGroupInput("visu_model_choice", label = "Choix des modèles", 
+                                           choices = list("CBOW 25d" = 1, "SkipGram 25d" = 2, "CBOW 500d" = 3, "SkipGram 500d" = 4),
+                                           selected = 1),
+                        fluidRow(column(width = 12,
+                                        checkboxInput("type_graph", label = "Regrouper les graphiques", value = F), style = "margin-top: 20px;"))
+                        ),
+                 column(width = 3, style = "margin-top: 93px;",
+                        actionButton("visu_button", "Valider"))),
+          column(width = 9,
                  plotOutput("visu_graph"))
         ))
   )
